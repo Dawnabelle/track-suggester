67310124c8f997e81b3cb5233c4919e3bcc211f5
Dawnabelle <dawn.mott@gmail.com>
2018-03-23 10:41:11 -0700

    add variables and prevent default to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("formOne").submit(function(event) {
+    var javaTrack = $("#javaTrack").val();
+    var cssTrack = $("#css-track").val();
+    var phpTrack = $("#php-track").val();
+
+
+
+    event.preventDefault();
+  });
+});
