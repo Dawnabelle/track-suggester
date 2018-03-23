f5d8935adbb258ae3d26e179c0eded1cb25aaf3e
Dawnabelle <dawn.mott@gmail.com>
2018-03-23 14:46:09 -0700

    remove questions and console.log for debugging

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
-  $("formOne").submit(function(event) {
-    var javaTrack = $("#javaTrack").val();
-    var cssTrack = $("#css-track").val();
-    var phpTrack = $("#php-track").val();
+  $("#formOne").submit(function(e) {
+    e.preventDefault();
+    var name = $("#name").val();
+    var age = $("#age").val();
+    var phpTrack1 = $("#php-track1").val();
+    var phpTrack2 = $("#php-track2").val();
+    var javaTrack1 = $("#java-track1").val();
+    var cssTrack1 = $("#css-track1").val();
+    console.log(name);
+    console.log(age);
+    console.log(phpTrack1);
+    console.log(phpTrack2);
+    console.log(javaTrack1);
+    console.log(cssTrack1);
 
-
-
-    event.preventDefault();
   });
 });
